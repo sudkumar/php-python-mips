8abb8c8c2f9b3776c19f31e7f6ea68110df520d5
Sudhir Mitharwal <luckysud4@gmail.com>
2016-02-02 10:40:43 +0530

    Flow graph added

--- asgn2/cgen/BBGen.py
+++ asgn2/cgen/BBGen.py
@@ -28,8 +28,9 @@ class BBGen():
 
     def __init__(self, ir):
         self._ir = ir.split("\n")           # get an array of lines
-        self._leaders = [1]                  # initialize the empty leaders array
-        countIRLines = len(self._ir)         # get the number of total lines in ir
+        self._leaders = [1]                 # initialize the empty leaders array
+        self._fns = {}                      # map the function label to it's leader's line number
+        countIRLines = len(self._ir)        # get the number of total lines in ir
 
         # loop through all the lines and, get and store all the `leaders` in leaders
         i = 0
@@ -41,29 +42,27 @@ class BBGen():
                 self.addLeader(int(lnParser._ln))    # add the first line to leaders
             # check for instruction type to be a label
             if(opType == IRInstructionTypes["label"]):
-                self.addLeader(int(lnParser._ln))       # it was a label, so is a `leader`
+                self._fns[lnParser.operands[0]] = int(lnParser._ln)
+                self.addLeader(lnParser._ln)       # it was a label, so is a `leader`
             
             # check for instruction type to be a jump
             elif(opType in JumpInstructions):
                 # get the jump target of this line, as it'll be a `leader`, must be an integer
-                try:
-                    x = int(lnParser.jumpTarget)
-                except ValueError, e:
-                    # raise e
-                    x = e
-                else:
-                    self.addLeader(int(lnParser.jumpTarget))
+                self.addLeader(lnParser.jumpTarget)
 
                 # it was a jump statement, so next line will be the `leader` if it exists
                 i += 1
                 if i < countIRLines:
                     lnParser = LineParser(self._ir[i])
-                    self.addLeader(int(lnParser._ln))  
+                    self.addLeader(lnParser._ln)  
 
                 else:
                     break                                   # reached at end of file
 
             i += 1      # go to next line
+
+        # finally sort the `leaders` 
+        self._leaders.sort() 
 
     def addLeader(self, leader):
         """Add a new leader
@@ -72,14 +71,22 @@ class BBGen():
         Arguments:
             leader {int} -- The new leader to be added
         """
-        # if leader is already not present in leaders, add it, else...
-        if not leader in self._leaders:
-            self._leaders.append(leader)
+
+        try:
+            leader = int(leader)
+        except ValueError, e:
+            # raise e
+            x = e
+        else:
+            # if leader is already not present in leaders, add it, else...
+            if not leader in self._leaders:
+                self._leaders.append(leader)
 
 
 if __name__ == '__main__':
     fp = open("sample_input2.ir", "r")
     bbGen = BBGen(fp.read())
     print bbGen._leaders
+    print bbGen._fns
 
 
