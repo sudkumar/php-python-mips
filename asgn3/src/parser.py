1c25e198f09c2205a2a103d72de680b022936bce
ssolanki <sahilsolanki07@gmail.com>
2016-02-29 16:15:31 +0530

    initial data structure for tree added

--- asgn3/src/parser.py
+++ asgn3/src/parser.py
@@ -3,36 +3,57 @@ import os, sys, lexer, logging, ply.yacc as yacc
 tokens = lexer.tokens
 
 precedence = (
-	('left', 'INCLUDE', 'INCLUDE_ONCE', 'EVAL', 'REQUIRE', 'REQUIRE_ONCE'),
-	('left', 'COMMA'),
-	('left', 'BIT_OR'),
-	('left', 'BIT_XOR'),
-	('left', 'BIT_AND'),
-	('left', 'EQUAL', 'PLUS_EQ','MINUS_EQ', 'MULTIPLY_EQ', 'DIVIDE_EQ', 'MOD_EQ', 'DOT_EQ'),
-	('left', 'COND_OP', 'COLON'),
-	('left', 'AND_OP', 'OR_OP'),
-	('left', 'BIT_RSHIFT', 'BIT_LSHIFT'),
-	('left', 'PLUS', 'MINUS', 'MULT', 'DIV', 'MOD', 'DOT'),
-	('left', 'ENDIF'),
-	('right', 'PRINT'),
-	('right', 'NOT'),
-	('right', 'BIT_NOT', 'INC', 'DEC',),
-	('right', 'LBRACKET'),
-	('right', 'STATIC', 'ABSTRACT', 'FINAL', 'PRIVATE', 'PROTECTED', 'PUBLIC'),
-	('nonassoc', 'EQ_EQ', 'IDENTICAL', 'NOT_IDENTICAL', 'LESSER', 'LESSER_EQ', 'GREATER', 'GREATER_EQ'),
-	('nonassoc', 'INSTANCEOF', 'NEW', 'CLONE'),
-	)
+  ('left', 'INCLUDE', 'INCLUDE_ONCE', 'EVAL', 'REQUIRE', 'REQUIRE_ONCE'),
+  ('left', 'COMMA'),
+  ('left', 'BIT_OR'),
+  ('left', 'BIT_XOR'),
+  ('left', 'BIT_AND'),
+  ('left', 'EQUAL', 'PLUS_EQ','MINUS_EQ', 'MULTIPLY_EQ', 'DIVIDE_EQ', 'MOD_EQ', 'DOT_EQ'),
+  ('left', 'COND_OP', 'COLON'),
+  ('left', 'AND_OP', 'OR_OP'),
+  ('left', 'BIT_RSHIFT', 'BIT_LSHIFT'),
+  ('left', 'PLUS', 'MINUS', 'MULT', 'DIV', 'MOD', 'DOT'),
+  ('left', 'ENDIF'),
+  ('right', 'PRINT'),
+  ('right', 'NOT'),
+  ('right', 'BIT_NOT', 'INC', 'DEC',),
+  ('right', 'LBRACKET'),
+  ('right', 'STATIC', 'ABSTRACT', 'FINAL', 'PRIVATE', 'PROTECTED', 'PUBLIC'),
+  ('nonassoc', 'EQ_EQ', 'IDENTICAL', 'NOT_IDENTICAL', 'LESSER', 'LESSER_EQ', 'GREATER', 'GREATER_EQ'),
+  ('nonassoc', 'INSTANCEOF', 'NEW', 'CLONE'),
+  )
+
+tree = [];
 
 def p_start(p):
     'start : top_statement_list'
+  
+    p[0] = p[1]
+    tree.append({"start":[p[1]]}) 
 
 def p_top_statement_list(p):
-	'''top_statement_list : top_statement_list top_statement
-	                      | empty '''
+    '''top_statement_list : top_statement_list top_statement
+                        | empty '''
+    if len(p) == 3 :
+        p[0] = [p[1],p[2]]        
+        # tree.top_statement_list = [p[1],p[2]]
+        tree.append({"top_statement_list":[p[1],p[2]]})
+    else:         
+        p[0] = p[1]
+        tree.append({"top_statement_list":[p[1]]})
 
 def p_top_statement(p):
-	'''top_statement : statement
-				     | function_declaration_statement'''
+    '''top_statement : function_declaration_statement
+    '''
+    p[0] = p[1]
+    tree.append({"top_statement":[p[1]]})
+
+
+def p_top_statement(p):
+    '''top_statement : statement'''
+    p[0] = p[1]
+    tree.append({"top_statement":[p[1]]})
+
 
 #--------------------------------------------------------------------------------------------------------                 
 
@@ -41,13 +62,30 @@ def p_top_statement(p):
 def p_top_statement_constant(p):
     'top_statement : VAR constant_declarations SEMICOLON'
 
+    # tree.p_top_statement = {p[1],p[2],p[3]}
+    p[0] =  [p[1],p[2],p[3]]
+    tree.append({"top_statement":[p[1],p[2],p[3]]})
+  
+
 def p_constant_declarations(p):
     '''constant_declarations : constant_declarations COMMA constant_declaration
                              | constant_declaration'''
-
+    if (len(p) == 4) :
+        p[0] = [p[1],p[2],p[3]]
+        tree.append({"constant_declarations":[p[1],p[2],p[3]]})
+    else:
+        p[0] = p[1]
+        tree.append({"constant_declarations":[p[1]]})
+         
 def p_constant_declaration(p):
     '''constant_declaration : IDENTIFIER EQUAL variable
-    					    | IDENTIFIER'''      
+                  | IDENTIFIER'''                                    
+    if (len(p) == 4) :
+        p[0] = [p[1],p[2],p[3]]
+        tree.append({"constant_declaration":[p[1],p[2],p[3]]})
+    else:
+        p[0] = p[1]
+        tree.append({"constant_declaration":[p[1]]})
 
 #--------------------------------------------------------------------------------------------------------                 
 
@@ -55,11 +93,25 @@ def p_constant_declaration(p):
 
 def p_function_declaration_statement(p):
     'function_declaration_statement : FUNCTION STRING LPAREN parameter_list RPAREN LBRACE inner_statement_list RBRACE'
+    p[0] = [p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8]]
+    tree.append({"function_declaration_statement":[p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8]]})
+
+def p_parameter_list(p):
+    '''parameter_list : empty'''    
+
+    p[0] = p[1]
+    tree.append({"function_declaration_statement":[p[1]]})
 
 def p_parameter_list(p):
     '''parameter_list : parameter_list COMMA parameter
-                      | parameter
-                      | empty'''
+                      | parameter'''
+    if(len(p)==2):                      
+        p[0] = p[1]
+        tree.append({"parameter_list":[p[1]]})
+    else:
+        p[0] = [p[1],p[2],p[3]]
+        tree.append({"parameter_list":[p[1],p[2],p[3]]})
+
 
 def p_parameter(p):
     '''parameter : IDENTIFIER
@@ -67,6 +119,19 @@ def p_parameter(p):
                  | IDENTIFIER EQUAL scalar
                  | BIT_AND IDENTIFIER EQUAL scalar'''
 
+    if(len(p)==2):                      
+        p[0] = p[1]
+        tree.append({"parameter":[p[1]]})
+    elif(len(p)==3):
+        p[0] = [p[1],p[2]]
+        tree.append({"parameter":[p[1],p[2]]})
+    elif(len(p)==4):
+        p[0] = [p[1],p[2],p[3]]
+        tree.append({"parameter":[p[1],p[2],p[3]]})
+    else:
+        p[0] = [p[1],p[2],p[3],p[4]]
+        tree.append({"parameter":[p[1],p[2],p[3],p[4]]})
+    
 #--------------------------------------------------------------------------------------------------------                 
 
 # --- IF ELSE
@@ -74,6 +139,7 @@ def p_parameter(p):
 def p_statement_if(p):
     '''statement : IF LPAREN expr RPAREN statement elseif_list else_single
                  | IF LPAREN expr RPAREN COLON inner_statement_list new_elseif_list new_else_single ENDIF SEMICOLON'''
+
 
 def p_elseif_list(p):
     '''elseif_list : empty
@@ -173,6 +239,9 @@ def p_case_list(p):
 def p_case_separator(p):
     '''case_separator : COLON
                       | SEMICOLON'''    
+    p[0] = p[1]
+    tree.append({"case_separator":[p[1]]})
+
 
 #--------------------------------------------------------------------------------------------------------                 
 
@@ -216,6 +285,13 @@ def p_static_var_list(p):
 def p_static_var(p):
     '''static_var : IDENTIFIER EQUAL scalar
                   | IDENTIFIER'''
+    if(len(p)==2):                  
+        p[0] = p[1]
+        tree.append({"static_var":[p[1]]})
+    else:
+        p[0] = [p[1],p[2],p[3]]
+        tree.append({"static_var":[p[1],p[2],p[3]]})
+
 #-------------
 
 #--- ECHO ----
@@ -236,10 +312,14 @@ def p_statement_block(p):
     'statement : LBRACE inner_statement_list RBRACE'
 
 def p_statement_empty(p):
-    'statement : SEMICOLON'                                                                                                                    
+    'statement : SEMICOLON'          
+    p[0] = p[1]                                                                                                              
+    tree.append({"statement":[p[1]]})
 
 def p_statement_expr(p):
-	'statement : expr SEMICOLON'
+    'statement : expr SEMICOLON'
+    p[0] = p[1]
+    tree.append({"statement":[p[1],p[2]]})
 
 def p_inner_statement_list(p):
     '''inner_statement_list : inner_statement_list inner_statement
@@ -247,21 +327,25 @@ def p_inner_statement_list(p):
 
 def p_inner_statement(p):
     '''inner_statement : statement
-    				   | function_declaration_statement'''
+               | function_declaration_statement'''
 
 #--------------------------------------------------------------------------------------------------------                 
 
 # --- expression defined
 
 def p_expr_variable(p):
-	'expr : variable'
+    'expr : variable'
+    p[0] = p[1]
+    tree.append({"expr":[p[1]]})
 
 def p_variable(p):
     '''variable : base_variable
-    			| function_call'''
+          | function_call'''
+    p[0] = p[1]
+    tree.append({"variable":[p[1]]})
 
 def p_function_call(p):
-	'function_call : STRING LPAREN function_call_parameter_list RPAREN'
+  'function_call : STRING LPAREN function_call_parameter_list RPAREN'
 
 
 def p_function_call_parameter_list(p):
@@ -276,19 +360,33 @@ def p_function_call_parameter(p):
 
 def p_reference_variable_array_offset(p):
     '''base_variable : base_variable LBRACKET dim_offset RBRACKET
-					 | base_variable LBRACE expr RBRACE
-    				 | IDENTIFIER'''
+           | base_variable LBRACE expr RBRACE
+             | IDENTIFIER'''
+    if(len(p)==2):
+        p[0] = p[1]
+        tree.append({"base_variable":[p[1]]})
+    else:
+        p[0] = [p[1],p[2],p[3],p[4]]
+        tree.append({"base_variable":[p[1],p[2],p[3],p[4]]})
 
 def p_dim_offset(p):
     '''dim_offset : expr
                   | empty'''
+    p[0] = p[1]
+    tree.append({"dim_offset":[p[1]]})
 
 def p_expr_assign(p):
     '''expr : variable EQUAL expr
             | variable EQUAL BIT_AND expr'''
-
+    if(len(p)==4):  
+        p[0] = [p[1],p[2],p[3]]            
+        tree.append({"expr":[p[1],p[2],p[3]]})
+    else:
+        p[0] = [p[1],p[2],p[3],p[4]]
+        tree.append({"expr":[p[1],p[2],p[3],p[4]]})
+        
 def p_expr_clone(p):
-	'expr : CLONE expr'
+  'expr : CLONE expr'
 
 def p_expr_list_assign(p):
     'expr : LIST LPAREN assignment_list RPAREN EQUAL expr'
@@ -304,12 +402,17 @@ def p_assignment_list_element(p):
 
 def p_expr_scalar(p):
     'expr : scalar'
+    p[0] = p[1]
+    tree.append({"expr":[p[1]]})
 
 def p_scalar(p):
     '''scalar : CONST_DECIMAL
-			  | CONST_DOUBLE
-			  | CONST_STRING
-			  | NULL'''    
+        | CONST_DOUBLE
+        | CONST_STRING
+        | NULL'''    
+    
+    p[0] = p[1]
+    tree.append({"scalar":[p[1]]})
 
 def p_expr_array(p):
     'expr : ARRAY LPAREN array_pair_list RPAREN'
@@ -321,6 +424,8 @@ def p_array_pair_list(p):
 def p_possible_comma(p):
     '''possible_comma : empty
                       | COMMA'''
+    p[0] = p[1]
+    tree.append({"expr":[p[1]]})
 
 def p_non_empty_array_pair_list_item(p):
     '''non_empty_array_pair_list : non_empty_array_pair_list COMMA BIT_AND variable
@@ -336,12 +441,14 @@ def p_non_empty_array_pair_list_pair(p):
 
 
 def p_expr_assign_op(p):
-	'''expr : variable PLUS_EQ expr
-    		| variable MINUS_EQ expr
+    '''expr : variable PLUS_EQ expr
+        | variable MINUS_EQ expr
             | variable MULTIPLY_EQ expr
             | variable DIVIDE_EQ expr
             | variable DOT_EQ expr
             | variable MOD_EQ expr'''
+    p[0] = [p[1],p[2],p[3]]
+    tree.append({"expr":[p[1],p[2],p[3]]})
 
 def p_expr_binary_op(p):
     '''expr : expr AND_OP expr
@@ -367,16 +474,22 @@ def p_expr_binary_op(p):
             | expr GREATER expr
             | expr GREATER_EQ expr
             | expr INSTANCEOF expr'''
+    p[0] = [p[1],p[2],p[3]]
+    tree.append({"expr":[p[1],p[2],p[3]]})
 
 def p_expr_unary_op(p):
     '''expr : PLUS expr
             | MINUS expr
             | BIT_NOT expr
             | NOT expr'''
+    p[0] = [p[1],p[2]]
+    tree.append({"expr":[p[1],p[2]]})
 
 def p_expr_ternary_op(p):
     'expr : expr COND_OP expr COLON expr'
-
+    p[0] = [p[1],p[2],p[3],p[4],p[5]]
+    tree.append({"expr":[p[1],p[2],p[3],p[4],p[5]]})
+    
 def p_expr_pre_incdec(p):
     '''expr : INC variable
             | DEC variable'''
@@ -387,7 +500,9 @@ def p_expr_post_incdec(p):
 
 def p_expr_empty(p):
     'expr : EMPTY LPAREN expr RPAREN'
-
+    p[0] = [p[1],p[2],p[3],p[4]]
+    tree.append({"expr":[p[1],p[2],p[3],p[4]]})
+    
 def p_expr_eval(p):
     'expr : EVAL LPAREN expr RPAREN'
 
@@ -407,26 +522,24 @@ def p_expr_require_once(p):
     'expr : REQUIRE_ONCE expr'
 
 def p_expr_exit(p):
-	'''expr : EXIT
-			| EXIT LPAREN RPAREN
-			| EXIT LPAREN expr RPAREN'''
+  '''expr : EXIT
+      | EXIT LPAREN RPAREN
+      | EXIT LPAREN expr RPAREN'''
 
 def p_expr_print(p):
-	'expr : PRINT expr'
+  'expr : PRINT expr'
 #--------------------------------------------------------------------------------------------------------                 
-
 
 def p_empty(p):
     'empty : '
+    p[0] = None
+    tree.append({"empty":['']})
 
 def p_error(t):
     if t:
         raise SyntaxError('invalid syntax', (None, t.lineno, None, t.value))
     else:
         raise SyntaxError('unexpected EOF while parsing', (None, None, None, None))
-
-
-
 
 parser = yacc.yacc(debug=True)
 
@@ -446,5 +559,5 @@ result = parser.parse(data,debug=log)
 
 print(result)
 
-
+print (tree)
 
